8468a8f133d1bde1b6ee5e942295579fd74ef429
sachintha <sachintha.rajith@gmail.com>
2013-05-18 21:03:24 +0530

    initial commit

---
+++ public/chat.js
@@ -0,0 +1,45 @@
+/**
+ * @author Sachintha
+ */
+$(document).ready(function(){
+	
+	var messages = [];
+    var socket = io.connect('http://localhost');
+   
+    socket.on('message', function (data) {
+        if(data.message) {
+            messages.push(data);
+            var html = '';
+            for(var i=0; i<messages.length; i++) {
+                html += '<b>' + (messages[i].username ? messages[i].username : 'Server') + ': </b>';
+                html += messages[i].message + '<br />';
+            }
+            $("#content").html(html);
+        } else {
+            console.log("There is a problem:", data);
+        }
+    });
+ 
+ 	$("#field").keyup(function(e) {
+        if(e.keyCode == 13) {
+            sendMessage();
+        }
+    });
+    
+    $("#send").click(function() {
+    	sendMessage();
+    });	
+	
+	function sendMessage() {
+		if(! $("#name").val()) {
+            alert("Please type your name!");
+        }else{
+        var text = field.value;
+        socket.emit('send', { message: text, username: $("#name").val() });
+        $("#field").val('');	
+        }
+        
+	}
+})
+    
+ 
