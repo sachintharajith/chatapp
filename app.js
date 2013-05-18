b43d995e883146522c7d70bd2d91b908e7cf14c2
sachintha <sachintha.rajith@gmail.com>
2013-05-18 21:09:38 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'),
+    routes  = require('./routes'),
+    port = 8000,
+    app = module.exports = express.createServer();
+
+// Configuration
+
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+
+//app.engine('jade', require('jade').__express);
+app.get("/", function(req, res){
+    res.render("index");
+});
+var io = require('socket.io').listen(app.listen(port));
+io.sockets.on('connection', function (socket) {
+    socket.emit('message', { message: 'welcome to the chat' });
+    socket.on('send', function (data) {
+    	console.log(data);
+        io.sockets.emit('message', data);
+    });
+});
+console.log("server runing on port " + app.get('port'));
